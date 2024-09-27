fe2ab8a7e9b0a2cfd86664eb3c0d07908a42524a
pc <pc@DESKTOP-GTIH8C9>
2024-09-27 10:38:26 +0200

    Ajoutez des fichiers projet.

---
+++ LocationCar/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocationCar.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ LocationCar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LocationCar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
